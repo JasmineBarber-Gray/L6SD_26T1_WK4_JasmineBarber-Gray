e5c46facb3b461b3ac003d6085ecfd381ff8ba0d
JasmineBarber-Gray <jasminebarbergray@gmail.com>
2026-02-23 21:16:44 +1300

    Notes

---
+++ W4D4-Linear&BinarySearch.py
@@ -0,0 +1,11 @@
+#what is Linear Search?
+#A linear search (or sequential search) is one of the simplest searching techniques. 
+#It works by checking each element in a list sequentially until the target value is found or the list ends. 
+#It does not require the data to be sorted and works on any data type.
+
+#How it works:
+#Start from the first element of the array.
+#Compare the current element with the target value.
+#If a match is found, return the index.
+#If no match is found after checking all elements, return -1.
+
